51c26cf9f6b86bbeeab1734b91c75a4d505316e6
admin <admin@adminui-MacBookPro.local>
2022-04-11 22:26:06 +0900

    react-query 기본 셋팅

--- react-query/src/index.js
+++ react-query/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={QueryClient}>
+      {/* devtools */}
+      <ReactQueryDevtools initialIsOpen={true}/>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
