9ea2c8d30d001cf3206f5ebfa66367d22af621a0
admin <admin@adminui-MacBookPro.local>
2022-04-12 23:34:05 +0900

    초기 셋팅 변경

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,9 +1,17 @@
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+
 
 function App() {
-  return (
-    <div>
-     test
-    </div>
+  const queryClient = new QueryClient();
+
+  return (    
+     <QueryClientProvider client={queryClient}>
+      <div>
+        테스트
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>  
   );
 }
 
--- react-query/src/index.js
+++ react-query/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {QueryClient, QueryClientProvider} from "react-query";
-import {ReactQueryDevtools} from "react-query/devtools";
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={QueryClient}>
-      {/* devtools */}
-      <ReactQueryDevtools initialIsOpen={true}/>
       <App />
-    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
