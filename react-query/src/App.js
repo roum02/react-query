afd8bc3716146bd71b1fd294cabf7b90e8699d68
admin <admin@adminui-MacBookPro.local>
2022-04-12 23:46:42 +0900

    react-query 공식문서 적용

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,18 +1,43 @@
-import {QueryClient, QueryClientProvider} from "react-query";
-import {ReactQueryDevtools} from "react-query/devtools";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import axios from "axios";
 
+const queryClient = new QueryClient();
 
-function App() {
-  const queryClient = new QueryClient();
-
-  return (    
-     <QueryClientProvider client={queryClient}>
-      <div>
-        테스트
-      </div>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>  
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   );
 }
 
-export default App;
+function Example() {
+  const { isLoading, error, data, isFetching } = useQuery("repoData", () =>
+    axios.get(
+      "https://api.github.com/repos/tannerlinsley/react-query"
+    ).then((res) => res.data)
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+      <div>{isFetching ? "Updating..." : ""}</div>
+      <ReactQueryDevtools initialIsOpen />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
