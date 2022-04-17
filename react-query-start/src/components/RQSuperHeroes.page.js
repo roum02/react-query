5c9dbbebd82dd78e1147369d35ea4bcfa7308779
roum02 <roum02@ewhain.net>
2022-04-17 22:21:59 +0900

    [Feat] Add query-key

--- react-query-start/src/components/RQSuperHeroes.page.js
+++ react-query-start/src/components/RQSuperHeroes.page.js
@@ -1,6 +1,25 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
 
 const RQSuperHeroes = () => {
-  return <div>RQSuperHeroes Page</div>;
+  const { isLoading, data } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
+
+  console.log(isLoading, data);
+
+  if (isLoading) {
+    return <h2>Loading....</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
 export default RQSuperHeroes;
