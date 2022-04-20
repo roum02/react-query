ecbed627a5e7228aab35a4534cffe3f24372a578
roum02 <roum02@ewhain.net>
2022-04-20 23:51:09 +0900

    [Feat] Modify Custom Hooks

---
+++ react-query-start/src/hooks/useSuperHerosData.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeros = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+export function useSuperHerosData(onSuccess, onError) {
+  return useQuery("super-heros", fetchSuperHeros, {
+    onSuccess,
+    onError,
+    select: (data) => {
+      const superHeroNames = data?.data.map((hero) => hero.name);
+      //return superHeroNames;
+      console.log(superHeroNames);
+    },
+  });
+}
